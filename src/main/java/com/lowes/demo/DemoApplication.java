294262339ccee8d3e96ff186b74f8c19442dd070
Sibgathulla <c8905666@lowes.com>
2020-09-23 18:19:15 +0530

    Spring Beans & Dependency Injection

--- src/main/java/com/lowes/demo/DemoApplication.java
+++ src/main/java/com/lowes/demo/DemoApplication.java
@@ -1,26 +1,23 @@
 package com.lowes.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RestController
-public class DemoApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
+public class DemoApplication {
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping(value="/")
-	public String hello() {
-		return "Hello world from Tomcat";
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return  new RestTemplate();
 	}
-	//Run mvn package - For packaging your application the war file be generated in the target directory
 }
