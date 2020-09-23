fc5374d6aa50f715a5bfe7fcc9822ecadd0e18a2
Sibgathulla <c8905666@lowes.com>
2020-09-23 19:52:29 +0530

    Use of @Value Annotation

--- src/main/java/com/lowes/demo/DemoApplication.java
+++ src/main/java/com/lowes/demo/DemoApplication.java
@@ -1,18 +1,21 @@
 package com.lowes.demo;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
+@RestController
+public class DemoApplication {
+	@Value("${spring.application.name}")
+	private String appName;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello world from Command Line Runner");
+	@RequestMapping(value = "/hello")
+	public String applicationName() {
+		return appName;
 	}
 }
