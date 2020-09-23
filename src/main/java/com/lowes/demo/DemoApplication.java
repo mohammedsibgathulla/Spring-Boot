182efaf87969482792da6a206a00fc09db908a90
Sibgathulla <c8905666@lowes.com>
2020-09-23 18:41:55 +0530

    Spring Boot Application Runner Interface

--- src/main/java/com/lowes/demo/DemoApplication.java
+++ src/main/java/com/lowes/demo/DemoApplication.java
@@ -1,23 +1,19 @@
 package com.lowes.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class DemoApplication {
-
-	@Autowired
-	RestTemplate restTemplate;
+public class DemoApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return  new RestTemplate();
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Hello World from Application Runner");
 	}
 }
