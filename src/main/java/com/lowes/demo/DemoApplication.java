9fd15ceb2f661d6151c2bca2d859a2c737dd540b
Sibgathulla <c8905666@lowes.com>
2020-09-24 17:33:02 +0530

    Spring Boot Logging

--- src/main/java/com/lowes/demo/DemoApplication.java
+++ src/main/java/com/lowes/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.lowes.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	@Value("${spring.application.name}")
 	private String appName;
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@RequestMapping(value = "/hello")
