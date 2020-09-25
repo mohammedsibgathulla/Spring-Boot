a112bbe2bd4c21f8cbd8e3770d6ba20ee3592455
Sibgathulla <c8905666@lowes.com>
2020-09-25 17:47:18 +0530

    Spring Boot Service Components

--- src/main/java/com/lowes/demo/controller/ProductController.java
+++ src/main/java/com/lowes/demo/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.lowes.demo.controller;
 
 import com.lowes.demo.exception.ProductNotFoundException;
 import com.lowes.demo.model.Product;
+import com.lowes.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,42 +13,29 @@ import java.util.Map;
 
 @RestController
 public class ProductController {
-    private static Map<String, Product> productRepo = new HashMap<>();
-    static {
-        Product honey = new Product();
-        honey.setId("1");
-        honey.setName("Honey");
-        productRepo.put(honey.getId(), honey);
 
-        Product almond = new Product();
-        almond.setId("2");
-        almond.setName("Almond");
-        productRepo.put(almond.getId(), almond);
+    @Autowired
+    ProductService productService;
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct() {
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+    }
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object>
+    updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+
+        productService.updateProduct(id, product);
+        return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
     }
     @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Object> delete(@PathVariable("id") String id) {
-        productRepo.remove(id);
+        productService.deleteProduct(id);
         return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
     }
-
-    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
-        if(!productRepo.containsKey(id))throw new ProductNotFoundException();
-        productRepo.remove(id);
-        product.setId(id);
-        productRepo.put(id, product);
-        return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
-    }
-
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     public ResponseEntity<Object> createProduct(@RequestBody Product product) {
-        productRepo.put(product.getId(), product);
+        productService.createProduct(product);
         return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
-    }
-
-    @RequestMapping(value = "/products")
-    public ResponseEntity<Object> getProduct() {
-        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
     }
 }
 
---
+++ src/main/java/com/lowes/demo/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.lowes.demo.service;
+
+import com.lowes.demo.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+@Service
+public class ProductService {
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+    public void createProduct(Product product) {
+        productRepo.put(product.getId(), product);
+    }
+    public void updateProduct(String id, Product product) {
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+    }
+    public void deleteProduct(String id) {
+        productRepo.remove(id);
+
+    }
+    public Collection<Product> getProducts() {
+        return productRepo.values();
+    }
+}
+
